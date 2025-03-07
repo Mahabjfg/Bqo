3a15ca9870081267e51c1921176f8c3e2dda1fb4
Mahabjfg <pkthebotmahabur@gmail.com>
2025-03-07 21:08:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+/**
+ * @author NTKhang
+ * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
+ * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
+ * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
+ *
+ * English:
+ * ! Please do not change the below code, it is very important for the project.
+ * It is my motivation to maintain and develop the project for free.
+ * ! If you change it, you will be banned forever
+ * Thank you for using
+ *
+ * Vietnamese:
+ * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
+ * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
+ * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
+ * Cảm ơn bạn đã sử dụng
+ */
+
 const { spawn } = require("child_process");
 const log = require("./logger/log.js");
-
-const PORT = process.env.PORT || 3000; // Set default port to 3000 if not provided
 
 function startProject() {
 	const child = spawn("node", ["Goat.js"], {
 		cwd: __dirname,
 		stdio: "inherit",
-		shell: true,
-		env: { ...process.env, PORT } // Pass PORT as an environment variable
+		shell: true
 	});
 
 	child.on("close", (code) => {
@@ -19,5 +35,4 @@ function startProject() {
 	});
 }
 
-log.info(`Starting Goat Bot on port ${PORT}...`);
 startProject();
